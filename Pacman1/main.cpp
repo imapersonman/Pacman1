b70b18a40fce82960f9fe742a0df75ec38f42ad7
Koissi Adjorlolo <imapersonman@koissis-mac-pro.local>
2016-02-22 12:45:33 -0800

    Initial Commit

---
+++ Pacman1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Pacman1
+//
+//  Created by Koissi Adjorlolo on 2/22/16.
+//  Copyright © 2016 centuryapps. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
